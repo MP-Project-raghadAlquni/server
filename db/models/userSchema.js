f6a554368fb20faf0941d2b93e82ba7d93198028
raghadquni <Raghad.ibrahim.elq.js@tuwaiq.edu.sa>
2021-12-26 16:56:20 +0300

    all user With New Readings

--- db/models/userSchema.js
+++ db/models/userSchema.js
@@ -18,6 +18,9 @@ const userSchema = new mongoose.Schema({
   doctor: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   patients: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   status: { type: mongoose.Schema.Types.ObjectId, ref: "Status" },
+  certificates: { type: Array },
+  letter: { type: String },
+  license: { type: String}
 });
 
 module.exports = mongoose.model("User", userSchema);
--- index.js
+++ index.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 const db = require("./db/index.js");
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 const roleRouter = require("./Routers/routes/role");
 app.use(roleRouter);
